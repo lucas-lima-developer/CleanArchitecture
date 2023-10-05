7638fc541124ec5ec95e21733966f6aa8ae6aec7
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-05 16:02:53 -0300

    adicionado caso de uso para deletar usuário

---
+++ CleanArchitecture.Application/UseCases/DeleteUser/DeleteUserResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Application.UseCases.DeleteUser
+{
+    public class DeleteUserResponse
+    {
+        public Guid Id { get; set; }
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+    }
+}
