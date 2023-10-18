8ef2b2c97d1b947cb6947571ef5b12205f7659e6
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-18 09:31:55 -0300

    criando classe helper que ira ajudar nas operações de usuário

---
+++ CleanArchitecture.Test/Helpers/UserHelper.cs
@@ -0,0 +1,32 @@
+﻿using CleanArchitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Test.Helpers
+{
+    public class UserHelper
+    {
+        public async static Task<User> CreateUser()
+        {
+            var dbContext = Factory.GetDatabaseContext();
+
+            var userCreated = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = "Lucas",
+                Email = "lucas@email.com",
+                DateCreated = DateTime.Now,
+                DateUpdate = DateTime.Now,
+                DateDeleted = DateTime.Now,
+            };
+
+            dbContext.Users.Add(userCreated);
+            await dbContext.SaveChangesAsync();
+
+            return userCreated;
+        }
+    }
+}
