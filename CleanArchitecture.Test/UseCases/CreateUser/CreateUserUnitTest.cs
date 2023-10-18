9ffe920710e8eb02780cd73b0bedf98273c6bfd6
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-18 10:18:33 -0300

    refatorando cteste unitario

---
+++ CleanArchitecture.Test/UseCases/CreateUser/CreateUserUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitecture.Application.UseCases.CreateUser;
+
+namespace CleanArchitecture.Test.UseCases.CreateUser
+{
+    public class CreateUserUnitTest
+    {
+        //[Fact]
+        //public async Task CreateUserHandler_WithCorrectData_ShouldReturnCreatedUserResponse()
+        //{
+        //    // Arrange
+
+        //    var hanlder = new CreateUserHandler();
+
+        //    // Act
+
+        //    // Assert
+        //}
+    }
+}
