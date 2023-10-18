c3af68ed53df5069b8485ef6a929065c74ee915b
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-17 21:30:46 -0300

    adicionando testes unitários do caso de uso que retorna todos os usuários

---
+++ CleanArchitecture.Test/Helpers/Helpers.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.UseCases.GetAllUser;
+using CleanArchitecture.Persistence.Context;
+using CleanArchitecture.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Test.Helpers
+{
+    public class Helpers
+    {
+        public static AppDbContext GetDatabaseContext()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "CleanArchitectureTest")
+                .Options;
+
+            var databaseContext = new AppDbContext(options);
+            databaseContext.Database.EnsureCreated();
+
+            return databaseContext;
+        }
+        public static Task<UserRepository> GetUserRepository()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "CleanArchitectureTest")
+                .Options;
+
+            var databaseContext = new AppDbContext(options);
+            databaseContext.Database.EnsureCreated();
+
+            return Task.FromResult(new UserRepository(databaseContext));
+        }
+
+        public static IMapper GetMapper()
+        {
+            var myProfile = new GetAllUserMapper();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+            IMapper mapper = new Mapper(configuration);
+
+            return mapper;
+        }
+    }
+}
---
+++ CleanArchitecture.Test/UseCases/GetAllUser/GetAllUserUnitTests.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using CleanArchitecture.Application.UseCases.GetAllUser;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Persistence.Context;
+using CleanArchitecture.Persistence.Repositories;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Test.UseCases.GetAllUser
+{
+    public class GetAllUserUnitTests
+    {
+        [Fact]
+        public async Task GetAllUserHandler_WithNoUserInDatabase_ShouldReturnEmptyArray()
+        {
+            // Arrange
+            var dbContext = await Helpers.Helpers.GetUserRepository();
+            var mapper = Helpers.Helpers.GetMapper();
+
+            var handler = new GetAllUserHandler(dbContext, mapper);
+
+            var request = new GetAllUserRequest { };
+
+            // Act
+            var result = await handler.Handle(request, new CancellationToken());
+
+            // Assert
+            result!.Count.Should().Be(0);
+            result.Should().BeEmpty();
+            result.Should().NotBeNull();
+            result.Should().BeOfType<List<GetAllUserResponse>>();
+        }
+
+        [Fact]
+        public async Task GetAllUserHandler_WithNoUser_ShouldReturnArrayWithUser()
+        {
+            // Arrange
+            var dbContext = Helpers.Helpers.GetDatabaseContext();
+            var userRepository = await Helpers.Helpers.GetUserRepository();
+            var mapper = Helpers.Helpers.GetMapper();
+
+            var userCreated = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = "Lucas",
+                Email = "lucas@email.com",
+                DateCreated = DateTime.Now,
+                DateUpdate = DateTime.Now,
+                DateDeleted = DateTime.Now,
+            };
+
+            dbContext.Users.Add(userCreated);
+            await dbContext.SaveChangesAsync();
+
+            var handler = new GetAllUserHandler(userRepository, mapper);
+
+            var request = new GetAllUserRequest { };
+
+            // Act
+            var result = await handler.Handle(request, new CancellationToken());
+
+            // Assert
+            result!.Count.Should().Be(1);
+            result.Should().NotBeNullOrEmpty();
+            result.Should().BeEquivalentTo(new List<GetAllUserResponse>
+            {
+                new GetAllUserResponse
+                {
+                    Id = userCreated.Id,
+                    Name = userCreated.Name,
+                    Email = userCreated.Email,
+                }
+            });
+            result.Should().BeOfType<List<GetAllUserResponse>>();
+        }
+    }
+}
