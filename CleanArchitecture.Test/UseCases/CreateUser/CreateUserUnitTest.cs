faab6c5cd022d29d2f4477df4b3224d54937afb4
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-18 11:04:33 -0300

    adicionado teste unitário que adiciona usuário ao banco de dados

--- CleanArchitecture.Test/UseCases/CreateUser/CreateUserUnitTest.cs
+++ CleanArchitecture.Test/UseCases/CreateUser/CreateUserUnitTest.cs
@@ -1,19 +1,42 @@
-﻿using CleanArchitecture.Application.UseCases.CreateUser;
+﻿using AutoMapper;
+using CleanArchitecture.Application.UseCases.CreateUser;
+using CleanArchitecture.Application.UseCases.GetAllUser;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Test.Helpers;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchitecture.Test.UseCases.CreateUser
 {
     public class CreateUserUnitTest
     {
-        //[Fact]
-        //public async Task CreateUserHandler_WithCorrectData_ShouldReturnCreatedUserResponse()
-        //{
-        //    // Arrange
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        //    var hanlder = new CreateUserHandler();
+        public CreateUserUnitTest()
+        {
+            _userRepository = Factory.GetUserRepository();
+            _mapper = Factory.GetMapper(new CreateUserMapper());
+            _unitOfWork = Factory.GetUnitOfWork();
+        }
 
-        //    // Act
+        [Fact]
+        public async Task CreateUserHandler_WithCorrectData_ShouldReturnCreatedUserResponse()
+        {
+            // Arrange
+            var context = Factory.GetDatabaseContext();
+            var handler = new CreateUserHandler(_unitOfWork, _userRepository, _mapper);
 
-        //    // Assert
-        //}
+            var request = new CreateUserRequest("newuser@email.com", "newuser");
+            // Act
+            var result = await handler.Handle(request, new CancellationToken());
+
+            // Assert
+            result.Name.Should().Be("newuser");
+            result.Email.Should().Be("newuser@email.com");
+            result.Id.Should().NotBeEmpty();
+            result.Should().BeOfType<CreateUserResponse>();
+        }
     }
 }
